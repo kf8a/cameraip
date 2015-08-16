6a9ed9845f95bb48554da17229ad7cb73f3e5d86
Sven Bohm <bohms@msu.edu>
2015-08-15 22:35:58 -0400

    start to instrument the camera ip collector

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
-	// "fmt"
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
 	"net/http"
 	"regexp"
+	"runtime"
 	"strings"
 	"time"
+)
+
+var (
+	ping_counter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "camera_pings",
+		Help: "Number of times the stats server was pinged.",
+	})
+	process_memory = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "process_TotalMem",
+		Help: "Total Memory used",
+	})
 )
 
 type camera struct {
@@ -27,6 +39,7 @@ func handler(cameras map[string]camera, w http.ResponseWriter, r *http.Request) 
 	match, _ := regexp.MatchString("g\\d+", camera_id)
 
 	if match {
+		ping_counter.Add(1)
 		ip := strings.Split(r.RemoteAddr, ":")[0]
 
 		c := camera{IP: ip, DateTime: time.Now()}
@@ -35,10 +48,21 @@ func handler(cameras map[string]camera, w http.ResponseWriter, r *http.Request) 
 	}
 }
 
+func process_stats() {
+	time.Sleep(1000)
+	m := &runtime.MemStats{}
+	runtime.ReadMemStats(m)
+	process_memory.Set(float64(m.Alloc))
+}
+
 func main() {
+	go process_stats()
+
 	cameras := make(map[string]camera)
 
 	r := mux.NewRouter()
+
+	r.Handle("/metrics", prometheus.Handler())
 
 	r.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
 		status(cameras, w, r)
